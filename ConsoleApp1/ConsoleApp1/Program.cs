f66a98f19339401ef493f8b38ce1549261921783
A-Tanner <ATanner5@ewu.edu>
2022-01-11 16:16:56 -0800

    Delete Program.cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Alt + arrow -> move highlighted rows up or down a level
+// ctrl+d -> format highlighted text
+class Program
+{
+
+    public void Main()
+    {
+        string text = "Inigo Montoya";
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(text);
+    }
+}
