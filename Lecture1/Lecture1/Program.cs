20bb56ea503e1db1cf4d58817161bb9d6d1f17d8
tnadalini <63377908+tnadalini@users.noreply.github.com>
2022-01-11 16:13:53 -0800

    bleh

---
+++ Lecture1/Lecture1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+
+    static void Main(String[] args)
+    {
+        string text = "bleh";
+
+        Guid guid = Guid.NewGuid();  
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Bingus" + text);
+    }
+
+
+}
+
