e3626762447e4a1284651c0ea299c74c49cdcf86
Bruce <bruce.gomes@intellitect.com>
2022-01-21 01:18:20 -0800

    initial FileLogger setup

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace Logger;
+
+public class FileLogger : BaseLogger
+{
+    private string FilePath { get; set; }
+
+    public FileLogger(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public void Log(LogLevel logLevel, string message)
+    {
+        // TODO: turn this in to a separate method
+        string newLogLine = "Test Line";
+        File.AppendAllLines(FilePath);
+    }
+
+}
