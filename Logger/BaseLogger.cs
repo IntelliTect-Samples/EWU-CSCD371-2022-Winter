397918d3a0c99baee998aa295fde34015a84d965
DakotaCondos <79940799+DakotaCondos@users.noreply.github.com>
2022-01-22 13:04:17 -0800

    Added auto property to hold class name in BaseLogger

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -3,5 +3,9 @@
     public abstract class BaseLogger
     {
         public abstract void Log(LogLevel logLevel, string message);
+
+        // There is an existing BaseLogger class. It needs an auto property to hold the class name.
+        public abstract string ClassName { get; set; }
+
     }
 }
