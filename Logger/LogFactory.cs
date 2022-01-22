9b976eed5b01e14350d5fec2ff4fd81fff3a1c3c
Bruce <bruce.gomes@intellitect.com>
2022-01-22 13:27:03 -0800

    LogFactory configs

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,10 +2,18 @@
 {
     public class LogFactory : BaseLogger
     {
+        private string FilePath { get; set; }
+
         public BaseLogger CreateLogger(string className)
         {
             ClassName = className;
-            return null;
+
+            return (string.IsNullOrEmpty(FilePath) ? null : new FileLogger(FilePath));
+        }
+
+        public void ConfigureFileLogger(string filePath)
+        {
+            FilePath = filePath;
         }
     }
 }
