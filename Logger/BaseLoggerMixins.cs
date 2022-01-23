bed3ca473c06648e42897ee079b21cf48ca3831e
Bruce <bruce.gomes@intellitect.com>
2022-01-23 02:15:01 -0800

    extension methods stub out

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,7 +1,40 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(this BaseLogger baseLogger, string message, string[] args)
+        {
+            if (baseLogger is null)
+                throw new ArgumentNullException("The BaseLogger reference can not be null");
+
+
+        }
+
+        public static void Warning(this BaseLogger baseLogger, string message, string[] args)
+        {
+            if (baseLogger is null)
+                throw new ArgumentNullException("The BaseLogger reference can not be null");
+
+
+        }
+
+        public static void Information(this BaseLogger baseLogger, string message, string[] args)
+        {
+            if (baseLogger is null)
+                throw new ArgumentNullException("The BaseLogger reference can not be null");
+
+
+        }
+
+        public static void Debug(this BaseLogger baseLogger, string message, string[] args)
+        {
+            if (baseLogger is null)
+                throw new ArgumentNullException("The BaseLogger reference can not be null");
+
+
+        }
+
     }
 }
