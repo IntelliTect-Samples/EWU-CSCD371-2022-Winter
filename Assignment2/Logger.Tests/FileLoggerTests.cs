ccd85a0991eb9ac91f9b6578e09db89245dfc2f6
Gaven Elder <gelder1@ewu.edu>
2022-01-24 21:09:29 -0800

    Implemented assignments conditions.

--- Assignment2/Logger.Tests/FileLoggerTests.cs
+++ Assignment2/Logger.Tests/FileLoggerTests.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logger.Mixins;
+using System;
 
 namespace Logger.Tests
 {
     [TestClass]
     public class FileLoggerTests
     {
-        
+     [TestClass]
+     public void TestLog()
+        {
+            //Arrange
+            FileLogger logger = new FileLogger 
+            {
+                filepath = "file.txt" //this is an empty file.
+            };
+            //Act
+            logger.Log(LogLevel.Debug, "message");
+
+            string[] test = System.IO.File.ReadAllText(logger.filepath);
+            //Assert
+            Assert.AreEqual("message", ResolveEventArgs[5].Trim());
+            Assert.AreEqual("Debug", ResolveEventArgs[4].Trim());
+        }
+
     }
 }
--- Assignment2/Logger.Tests/LogFactoryTests.cs
+++ Assignment2/Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,28 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+        [TestMethod]
+        public void loggerconfignull()
+        {
+            // Arrange
+            LogFactory fact = new LogFactory();
+            // Act
+            FileLogger test = (FileLogger) fact.CreateLogger("","null");
+            // Assert
+            Assert.AreEqual(null, test);
+        }
+
+        [TestMethod]
+        public void loggerfromfact()
+        {
+            // Arrange
+            LogFactory fact = new LogFactory();
+            // Act
+            FileLogger test = (FileLogger) fact.CreateLogger("class","file.txt");
+            // Assert
+            Assert.AreEqual(test.className, "class");
+            Assert.AreEqual(test.filePath, "file.txt");
+
+        }
     }
 }
