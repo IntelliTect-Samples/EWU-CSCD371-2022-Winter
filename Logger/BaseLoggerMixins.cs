130a7de49c4d64348334a2c373e0f90486a88b7e
Pedro H <phernandez5@eagles.ewu.edu>
2022-01-25 21:47:54 -0800

    More changes

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,25 +1,38 @@
-﻿namespace Logger
+﻿using System;
+namespace Logger
 {
     public static class BaseLoggerMixins
     {
         
-        public class void Error()
+        public class void Error(this BaseLogger logger, string message, params object[] list)
         {
-
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
         }
 
-        public class void Warning()
+        public class void Warning(this BaseLogger logger, string message, params object[] list)
         {
-
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
         }
 
-        public class void Information()
+        public class void Information(this BaseLogger logger, string message, params object[] list)
         {
-
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
         }
-        public class void Debug()
+        public class void Debug(this BaseLogger logger, string message, params object[] list)
         {
-
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
         }
 
     }
