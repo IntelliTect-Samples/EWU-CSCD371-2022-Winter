f9ff64c4fabf86a5a73901b64bd0186ee25b2f8e
Pedro H <phernandez5@eagles.ewu.edu>
2022-01-31 17:52:38 -0800

    Assignment3

---
+++ CanHazFunny/CanHazFunny/IJokeOutput.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CanHazFunny
+{
+    public interface IJokeOutput
+    {
+        void PrintJoke(string joke);
+    }
+}
---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CanHazFunny
+{
+    public class Jester
+    {
+        private JokeService jokeService;
+        private JokeOutput jokeOutput;
+
+        public Jester(JokeService jokeService, JokeOutput jokeOutput)
+        {
+            this.jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+            this.jokeOutput = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
+        }
+
+        public void TellJoke()
+        {
+            string joke = jokeService.GetJoke();
+
+            while (joke.Contains("Chuck Norris", StringComparison.OrdinalIgnoreCase))
+                joke = jokeService.GetJoke();
+
+            jokeOutput.PrintJoke(joke);
+        }
+    }
+}
