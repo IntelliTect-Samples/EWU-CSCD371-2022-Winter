162be4dfc33dbb547d4fdb660f8e501e3fece0da
tnadalini <63377908+tnadalini@users.noreply.github.com>
2022-02-01 14:07:16 -0800

    More JesterTests created

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,71 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        //egg
+        private JokeService _jokeService;
+        private PrintService _printService;
+        private Jester _jester;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _jokeService = new JokeService();
+            _printService = new PrintService();
+        }
+
+
+        [TestMethod]
+        public void ValidJokeService_GetJoke_ReturnsNonNull()
+        {
+            string joke = _jokeService.GetJoke();  
+            Assert.IsNotNull(joke);
+        }
+
+
+        [TestMethod]
+        public void ValidJokeServiceAndPrintSercive_ReturnsNonNullJester()
+        {
+            _jester = new Jester(_jokeService, _printService);
+
+            Assert.IsNotNull(_jester);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullJokeService_ExpectedNULLException_JesterCreation()
+        {
+            _jester = new Jester(null, _printService);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullPrintService_ExpectedNULLException_JesterCreation()
+        {
+            _jester = new Jester(_jokeService, null);
+        }
+
+
+        [TestMethod]
+        public void CheckJokeString_DoesNotContain_ChuckNorris()
+        {
+            //the code below is an exact copy of Jester.TellJoke method
+            //this TestMethod is meant to make sure this Jester.TellJoke method is working properly
+            string joke = "Chuck Norris"; //purposefully bad
+
+            do
+            {
+                joke = _jokeService.GetJoke();
+            }
+            while (joke.Contains("Chuck Norris", StringComparison.OrdinalIgnoreCase));
+
+            Assert.IsFalse(joke.Contains("Chuck Norris"));
+        }
     }
 }
---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny
+{
+    public class Jester
+    {
+        private JokeService jokeService;
+        private PrintService printService;
+
+        public Jester(JokeService jokeServ, PrintService printServ)
+        {
+            jokeService = jokeServ ?? throw new ArgumentNullException(nameof(jokeServ));
+            printService = printServ ?? throw new ArgumentNullException(nameof(printServ));
+        }
+
+        
+        public void TellJoke()
+        {
+            string joke = "Chuck Norris"; // purposefully bad
+
+            do
+            {
+                joke = jokeService.GetJoke();
+            }
+            while (joke.Contains("Chuck Norris", StringComparison.OrdinalIgnoreCase));
+
+            printService.Print(joke);
+        }
+
+    }
+}
