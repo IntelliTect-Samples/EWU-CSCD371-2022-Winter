10327bad2ebfb837b587de52686b195ec15dfe34
A-Tanner <atanner5@ewu.edu>
2022-02-05 14:37:49 -0800

    Adds tests with 98.9% coverage

--- GenericsHomework/CircularlyLinkedList.Tests/CircularlyLinkedListTests.cs
+++ GenericsHomework/CircularlyLinkedList.Tests/CircularlyLinkedListTests.cs
@@ -1,15 +1,198 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
 
 namespace GenericsHomework.Tests
 {
     [TestClass]
-    public class CircularlyLinkedList
+    public class CircularlyLinkedListTests
     {
+        int[] testIntData = { 1, 7, 22, 48 };
+        string[] testStringData = { "Diego", "Karolyn", "Meatball Supreme" };
+        string?[] testNullData = { "The", "Data", null, "Big" };
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void Exists_IfDataNotInList_ReturnsFalse()
         {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            Assert.IsFalse(cll.Exists(42));
         }
+
+        [TestMethod]
+        public void Exists_IfDataInList_ReturnsTrue()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            Assert.IsTrue(cll.Exists(testIntData[2]));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddData_IfDuplicateData_ThrowsException()
+        {
+            CircularlyLinkedList<string> cll = new();
+            cll.AddData("fred");
+            cll.AddData("fred");
+        }
+        [TestMethod]
+        public void AddData_IncrementsCoundt()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            Assert.AreEqual<int>(testIntData.Length, cll.Count);
+        }
+
+        [TestMethod]
+        public void AddData_AddsData()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+
+            foreach (int element in testIntData)
+            {
+                Assert.IsTrue(cll.Exists(element));
+            }
+        }
+
+        [TestMethod]
+
+        public void AddData_RejectsNull()
+        {
+            CircularlyLinkedList<string?> cll = new();
+
+            cll.AddData(testNullData);
+
+            foreach (string? element in testNullData)
+            {
+                if (element is not null)
+                {
+                    Assert.IsTrue(cll.Exists(element));
+                }
+            }
+
+            Assert.AreEqual<int>(3, cll.Count);
+
+        }
+        [TestMethod]
+        public void AddData_IfCursorIsNull_AddsDataInStoredOrder()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+
+            for (int i = 0; i < cll.Count; i++)
+            {
+                Assert.AreEqual<int>(testIntData[i], cll[i]);
+            }
+        }
+        [TestMethod]
+        public void AddData_IfCursorExists_AddsDataInStoredOrder()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(2022);
+            cll.AddData(testIntData);
+
+            for (int i = 0; i < testIntData.Length; i++)
+            {
+                Assert.AreEqual<int>(testIntData[i], cll[i + 1]);
+            }
+        }
+        [TestMethod]
+
+        public void GetStringOfIndex_ReturnsBaseObjectToString()
+        {
+            Object[] mockData = new Mock<Object>[3];
+
+            for (int i = 0; i < mockData.Length; i++)
+            {
+                mockData[i] = new Mock<Object>();
+
+            }
+
+            CircularlyLinkedList<Object> cll = new();
+            cll.AddData(mockData);
+
+            for (int i = 0; i < cll.Count; i++)
+            {
+                Assert.AreEqual<string>(mockData[i].ToString()!, cll.GetStringOfIndex(i));
+            }
+
+        }
+
+        [TestMethod]
+        public void SetOrigin_ChangesCursorPosition()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            cll.SetOrigin(2);
+            Assert.AreEqual<int>(testIntData[2], cll[0]);
+        }
+
+        [TestMethod]
+        public void SetOrigin_ChangesCursorPosition_HandlesNegatives()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            cll.SetOrigin(-1);
+            Assert.AreEqual<int>(testIntData[3], cll[0]);
+        }
+
+        [TestMethod]
+        public void Clear_RemovesAllNodesButCursor()
+        {
+            CircularlyLinkedList<int> cll = new();
+            cll.AddData(testIntData);
+            cll.SetOrigin(-1);
+            cll.Clear();
+            Assert.AreEqual<int>(testIntData[3], cll[0]);
+            Assert.AreEqual<int>(1, cll.Count);
+            Assert.AreEqual<int>(cll[0], cll[1]); //If cll has a single node, cll[1] still represents the adjacent node, which would be itself
+        }
+
+        [TestMethod]
+        public void Clear_ProperlyGarbageCollects()
+        {
+            CLLTest<int> cll = new();
+            cll.AddData(testIntData);
+            WeakReference? outRef;
+            outRef = cll.Clear();
+            Assert.IsNotNull(outRef);
+            GC.Collect();
+            Assert.IsFalse(outRef.IsAlive);
+        }
+
+
     }
 
+    //Summary:
+    //
+    //An extension of CircularlyLinkedLists to test garbage collection of Clear().
 
+    internal class CLLTest<T> : GenericsHomework.CircularlyLinkedList<T>
+    {
+        //Summary:
+        //
+        //Performs operations on a linked list identical to CircularlyLinkedList.Clear()
+        //Returns a weak reference to a node that should theoretically be garbage collected after Clear()
+        new public WeakReference? Clear()
+        {
+            if (_Cursor is not null)
+            {
+
+                WeakReference nodeRef = new(_Cursor.Next);
+                _Cursor.SetNext(_Cursor);
+                return nodeRef;
+
+            }
+            else return null;
+        }
+
+    }
 }
+
+
+
+
+
+
+
--- GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
+++ GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
@@ -3,36 +3,68 @@
 namespace GenericsHomework;
 public class CircularlyLinkedList<T>
 {
-    private Node<T>? _Cursor { get; set; }
+    protected Node<T>? _Cursor { get; set; }
 
-    public int Count { get; private set; } = 0;
+    public int Count { get; protected set; } = 0;
 
     public T this[int index]
     {
         get =>  ReturnDataAtIndex(index);
     }
+
+    public string GetStringOfIndex(int index)
+    {
+        if (_Cursor is null) { throw new InvalidOperationException($"{nameof(GetStringOfIndex)} called when there were no nodes in the list"); }
+        Node<T> origin = _Cursor;
+        SetOrigin(index);
+        string retString = _Cursor.ToString();
+        _Cursor = origin;
+        return retString;
+    }
     
     //Summary :
     //Adds information from an array of type T to the linked list
-    //The array will have the appearance of being "loaded backwards," 
-    //for example, an ascended array will be loaded as a descending array on traversal
+    //Starts at the end of an array and "adds backwards"
+    //The end result is the the information in the linked list has the same order as in the array
+    //
+    //data[0] will be stored in _Cursor if the linked list is empty,
+    //or in _Cursor.next if the linked list has at least one node
     public void AddData(params T[] data)
     {
-        for (int i = data.Length-1; i >= 0; i--)
+        if (_Cursor is null) 
         {
-            if (data[i] is not null) Insert(data[i]);
+            Insert(data[0]);
+            for (int i = data.Length - 1; i > 0; i--)
+            {
+                if (data[i] is not null) Insert(data[i]);
 
-            else Console.WriteLine($"{nameof(AddData)}: index{i} is null and was rejected");
+                else Console.WriteLine($"{nameof(AddData)}: index{i} is null and was rejected");
 
+            }
+        }
+        else
+        {
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                if (data[i] is not null) Insert(data[i]);
+
+                else Console.WriteLine($"{nameof(AddData)}: index{i} is null and was rejected");
+
+            }
         }
     }
+    
 
     private void Insert(T data) { 
         if (data is null) throw new ArgumentNullException($"{nameof(Insert)} should never receive null");
+        if (Exists(data)) throw new ArgumentException($"{data} exists in the circularly linked list");
+
     
         if (_Cursor is null) _Cursor = new Node<T>(data);
        
         else _Cursor.Append(data);
+
+        Count++;
         
     }
 
@@ -60,73 +92,66 @@ public class CircularlyLinkedList<T>
 
     //Summary:
     //
-    // Deletes all nodes except the node who calls this method (referred to as _Cursor)
-    //  Does this by traversing to the node behind the _Cursor, setting it's Next reference to be the node after _Cursor,
-    //  setting _Cursor to it's original node, and then setting _Cursor's next to be a reference to itself.
+    //Deletes all nodes but the one pointed to by Cursor
     //
-    // Since Next is a non-nullable property, the nodes to be deleted must reform a circular node formation.
-    //  However, as there is no access to this circular node formation from a root (such as a variable or static member,)
-    //  garbage collection will safely dispose of the circular node formation created in this function
+    //This works because the garbage collector will trace the references in the root graph
+    //Since only _Cursor populates the root graph, only the nodes accessible to _Cursor will remain uncollected by the GC
+    //Meaning that only _Cursor remains uncollected. The nodes that originally existed in the linked list, not being accessible in the root graph
+    //will all be garbage collected, even though one points to _Cursor
     public void Clear()
     {
         if (_Cursor is not null)
         {
-
-            Node<T> head = _Cursor!;
-
-            while (_Cursor.Next != head)
-            {
-                StepForward();
-            }
-
-            _Cursor.SetNext(head.Next);
-            _Cursor = head;
-            _Cursor.SetNext(head);
+            _Cursor.SetNext(_Cursor);
             Count = 1;
         }
     }
-    private void StepForward()
+
+    //Summary:
+    // Method used to move Cursor to a new index
+    public void SetOrigin(int Index)
+    {
+        int distance = Index % Count;
+
+        if (distance < 0) distance += Count;
+
+        for (; distance != 0; distance--)
+        {
+            StepForward();
+        }
+    }
+    protected void StepForward()
     {
         if (_Cursor is not null)
             _Cursor = _Cursor.Next;
     }
 
 
-    private void StepTo(int Index)
-    {
-        int distance = Index % Count;
-
-        if (distance < 0) distance += Count;
-
-        for (; distance!= 0; distance--)
-        {
-            StepForward();
-        }
-    }
 
     //Summary:
     //
     // Returns data from the node index steps away from the current selected node.
     //  Handles negative values on a "index step backwards" basis
     //  accessed by indexer
-    private T ReturnDataAtIndex(int index)
+    protected T ReturnDataAtIndex(int index)
     {
         if (_Cursor is null) { throw new InvalidOperationException($"{nameof(ReturnDataAtIndex)} called when there were no nodes in the list"); }
         Node<T> origin = _Cursor;
-        StepTo(index);
+        SetOrigin(index);
         T retVal = _Cursor.Data;
         _Cursor = origin;
         return retVal;
     }
 
-    private class Node<Tdata>
+    protected class Node<Tdata>
     {
-        public Node(Tdata data)
+        public Node([DisallowNull] Tdata data)
         {
             Data = data;
             Next = this;
         }
 
+        [DisallowNull]
         public Tdata Data { get; private set; }
 
         [NotNull]
@@ -140,6 +165,7 @@ public class CircularlyLinkedList<T>
 
         public void Append([DisallowNull]Tdata nodeData)
         {
+            
             Node<Tdata> temp = Next;
             Node<Tdata> addedNode = new(nodeData) { Next = temp };
             Next = addedNode;
