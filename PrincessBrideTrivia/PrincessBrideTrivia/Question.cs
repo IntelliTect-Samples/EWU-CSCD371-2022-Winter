16d42feded5c64c7ec98147a054e23aa0843fbd7
Ryan Breithaupt <rbreithaupt@ewu.edu>
2022-02-07 16:58:21 -0800

    Did the best i could so far

--- PrincessBrideTrivia/PrincessBrideTrivia/Question.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Question.cs
@@ -5,5 +5,12 @@ namespace PrincessBrideTrivia
         public string Text;
         public string[] Answers;
         public string CorrectAnswerIndex;
+
+        public Question(string Q, string[] A, string a)
+        {
+            this.Text = Q;
+            this.Answers = A;
+            this.CorrectAnswerIndex = a;
+        }
     }
 }//Hi there buddy
