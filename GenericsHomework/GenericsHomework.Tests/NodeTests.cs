93986f20fa5e2a1824040990d32f2cd679b4017b
tnadalini <63377908+tnadalini@users.noreply.github.com>
2022-02-08 17:58:58 -0800

    Node class & NodeTests

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomework.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    public Node<object>? _Node;
+
+
+    [TestInitialize]
+    public void Init()
+    {
+        Node<object> nn = new Node<object>(1);
+        _Node = nn;
+    }
+
+    [TestMethod]
+    public void TestToStringReturnsCorrectly()
+    {
+        Assert.AreEqual<object>(_Node!.ToString(), "1");
+    }
+}
---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,74 @@
+﻿namespace GenericsHomework;
+
+using System;
+public class Node<T> : IComparable<Node<T>>
+{
+    private Node<T> _Next;
+    public T? Data { get; private set; }
+
+    public Node<T> Next
+    {
+        get { return _Next; }
+        private set { _Next = value; }
+    }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        return Data?.ToString();
+    }
+
+    public bool Exists(T data, Node<T> root)
+    {
+        Node<T> current = root;
+
+        while (current.Next != current && current.Next != root)
+        {
+            if (new Node<T>(data).CompareTo(current) == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void Clear()
+    {
+        Node<T> head = this;
+        Node<T> current = this;
+
+        while (current.Next != current && current.Next != head)
+        {
+            current = current.Next;
+        }
+
+        if (current.Next == current)
+        {
+            head.Next = head;
+        }
+
+        else
+        {
+            current.Next = current;
+            head.Next = head;
+        }
+
+    } //end Clear
+
+
+
+    public int CompareTo(Node<T>? node)
+    {
+        return this.CompareTo(node);
+    } //end CompareTo
+
+
+
+
+}
