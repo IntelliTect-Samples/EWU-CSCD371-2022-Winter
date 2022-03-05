7bb55edc186f132040eae0d691779ba322eada4b
Grant Erickson <grant@granterickson.com>
2022-03-04 21:54:26 -0800

    A solution

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class SampleDataTests
+    {
+        private SampleData Sut = new SampleData("People.csv");
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            Assert.AreEqual(50, Sut.CsvRows.Count());
+        }
+
+        [TestMethod]
+        public void Csv()
+        {
+            Assert.AreEqual("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", Sut.CsvRows.First());
+        }
+
+        [TestMethod]
+        public void UniqueStates()
+        {
+            Assert.AreEqual(27, Sut.GetUniqueSortedListOfStatesGivenCsvRows().Count());
+            Assert.AreEqual("AL", Sut.GetUniqueSortedListOfStatesGivenCsvRows().First());
+            Assert.AreEqual("WV", Sut.GetUniqueSortedListOfStatesGivenCsvRows().Last());
+        }
+
+        [TestMethod]
+        public void People()
+        {
+            Assert.AreEqual(50, Sut.People.Count());
+            Assert.AreEqual("Arthur", Sut.People.First().FirstName);
+            Assert.AreEqual("Amelia", Sut.People.Last().FirstName);
+            // Make sure some tricky ones in the middle are in the right spots.            
+            var indexOfFayette = Sut.People.Select(f => f.FirstName).ToList().IndexOf("Fayette");
+            var indexOfJoly = Sut.People.Select(f => f.FirstName).ToList().IndexOf("Joly");
+            Assert.AreEqual(indexOfFayette, indexOfJoly - 1);
+        }
+
+        [TestMethod]
+        public void StateCsvList()
+        {
+            Assert.AreEqual("AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV", Sut.GetAggregateListOfStatesGivenPeopleCollection(Sut.People));
+            Assert.AreEqual("AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV", Sut.GetAggregateSortedListOfStatesUsingCsvRows());
+        }
+
+        [TestMethod]
+        public void Filter()
+        {
+            Assert.AreEqual(5, Sut.FilterByEmailAddress(f => f.Contains(".gov")).Count());
+        }
+
+        [TestMethod]
+        public void GroupedByState()
+        {
+            Assert.AreEqual(27, Sut.GetPeopleGroupedByState().Count());
+            Assert.AreEqual(3, Sut.GetPeopleGroupedByState().First(f=>f.Key == "WA").Count());
+
+        }
+    }
+
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,30 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private Dictionary<string, Person> _data;
+        private List<string> _csv;
+        public SampleData(string filename)
+        {
+            var content = System.IO.File.ReadAllText(filename);
+            // List of csv strings
+            _csv = new List<string>(content.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Skip(1));
+            // Get this into an list of arrays
+            var splitData = new List<string[]>(_csv.Select(f => f.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)));
+            // Convert to dictionary
+            _data = new Dictionary<string, Person>(splitData
+                .Select(f => new KeyValuePair<string, Person>(f[0], new Person(
+                    firstName: f[1],
+                    lastName: f[2],
+                    address: new Address(streetAddress: f[4], city: f[5], state: f[6], zip: f[7]),
+                    emailAddress: f[3]
+                )))
+            );
+
+        }
+
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows => _csv;
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+            => _data.Values.Select(f => f.Address.State).Distinct().OrderBy(f => f);
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+            => string.Join(",", GetUniqueSortedListOfStatesGivenCsvRows());
 
         // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+        public IEnumerable<IPerson> People => _data.Values
+            .OrderBy(f => f.Address.State)
+            .ThenBy(f => f.Address.City)
+            .ThenBy(f => f.Address.Zip)
+            .ThenBy(f => f.Address.StreetAddress);
 
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
+            Predicate<string> filter) => _data.Values
+            .Where(f => filter(f.EmailAddress))
+            .Select(f => (f.FirstName, f.LastName));
 
         // 6.
         public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
+            IEnumerable<IPerson> people) =>
+            String.Join(",", people.Select(f => f.Address.State).Distinct());
+
+        public IEnumerable<IGrouping<string,Person>> GetPeopleGroupedByState() => _data.Values.GroupBy(f => f.Address.State);
+        
     }
 }
